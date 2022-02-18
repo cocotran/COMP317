56cee1bb0425b608e4bd058d2b333d44f57907e7
Quang Anh Tran <anhquangtran2703@gmail.com>
2022-02-17 22:38:14 -0500

    Add state comparison functions and objective function

--- a4/Provided/coloredTiles.py
+++ a4/Provided/coloredTiles.py
@@ -15,10 +15,10 @@
 import random as rand
 import math as math
 
+
 class State(object):
-    """The Problem State is an array of Boolean values, which we represent by a nested list.  True means a green tile, False a red tile.
-       
-    """
+    """The Problem State is an array of Boolean values, which we represent by a nested list.  True means a green tile, False a red tile."""
+
     def __init__(self, puzzle, touchpoints):
         """
         Initialize the State object.
@@ -33,49 +33,65 @@ class State(object):
         self.puzzle = puzzle
         self.nrows = len(self.puzzle)
         self.ncols = len(self.puzzle[0])
-        
+
         # The touchpoints are the main part of the state, so do make a copy
         self.touchpoints = [x for x in touchpoints]
-        
-       
-                     
+
     def __str__(self):
-        """ A string representation of the State """
-        return '<{} {}>'.format(str(self.puzzle), str(self.touchpoints))
+        """A string representation of the State"""
+        return "<{} {}>".format(str(self.puzzle), str(self.touchpoints))
 
     def __eq__(self, other):
-        """ Defining this function allows states to be compared
-        using the == operator """        
+        """Defining this function allows states to be compared
+        using the == operator"""
         return self.puzzle == other.puzzle and self.touchpoints == other.touchpoints
-        
-    
+
     def is_better_than(self, other):
-        """ 
-            Return True if self is a better solution than other
-            :param: other: a State object
-            :return: boolean
         """
-        #TODO: Implement this
-        return False
+        Return True if self is a better solution than other
+        :param: other: a State object
+        :return: boolean
+        """
+        count = 0
+        for row in range(self.nrows):
+            for col in range(self.ncols):
+                if self.puzzle[row][col]:
+                    count += 1
+                if other[row][col]:
+                    count -= 1
+
+        if count == 0:
+            # The number of red tiles is equal
+            return len(self.touchpoints) < len(other.touchpoints)
+
+        return count > 0
 
     def is_equal_to(self, other):
-        """ 
-            Return True if self is as good a solution as other
-            :param: other: a State object
-            :return: boolean
         """
-        #TODO: Implement this
+        Return True if self is as good a solution as other
+        :param: other: a State object
+        :return: boolean
+        """
+        count = 0
+        for row in range(self.nrows):
+            for col in range(self.ncols):
+                if self.puzzle[row][col]:
+                    count += 1
+                if other[row][col]:
+                    count -= 1
+
+        if count == 0:
+            # The number of red tiles is equal
+            return len(self.touchpoints) == len(other.touchpoints)
+
         return False
-        
-    
+
 
 class Problem(object):
-    """The Problem class defines aspects of the problem.
-
-    """
+    """The Problem class defines aspects of the problem."""
 
     def __init__(self, nrows, ncols, start=None):
-        """ The problem is defined by an initial grid of tiles.
+        """The problem is defined by an initial grid of tiles.
         It is assumed the goal state is an all-green grid.
 
             :param nrows: number of rows in the tile puzzle
@@ -87,11 +103,11 @@ class Problem(object):
         # convert from strings to nested list of booleans
         # in order to create a State
         # initially, there are no touch points
-        start = [[col=="G" for col in row] for row in start]
-        
-        
-        if start is not None: self.init_state = State(start, [])
-        
+        start = [[col == "G" for col in row] for row in start]
+
+        if start is not None:
+            self.init_state = State(start, [])
+
         # legal actions based only on dimensions, so cache actions in advance for performance
         self.actions_cache = []
         for r in range(self.nrows):
@@ -99,67 +115,78 @@ class Problem(object):
                 # actions are just a coordinate pair for the square being touched
                 a = (r, c)
                 self.actions_cache.append(a)  # left
-            
 
     def create_initial_state(self):
-        """ returns an initial state.
-            Here, we return the stored initial state.
+        """returns an initial state.
+        Here, we return the stored initial state.
         """
         return self.init_state
-        
+
     def objective_function(self, state):
         """
-            returns the value of the given state according
-            to the objective function
+        returns the value of the given state according
+        to the objective function
         """
-        #TODO: Implement this
-        return 0
-        
+        count = 0
+        for row in state.puzzle:
+            for tile in row:
+                if not tile:  # count red tiles
+                    count += 1
+        steps = 1 / len(state.touchpoints)
+        return count - steps
+
     def random_state(self):
-        """ Return a random State, completely independent of any other State.
-        """
-        #TODO: Implement this
-        return None
-           
+        """Return a random State, completely independent of any other State."""
+        random_puzzle = []
+        random_touchpoints = []
+
+        for r in range(self.nrows):
+            new_row = []
+            for c in range(self.ncols):
+                new_row.append(bool(rand.randint(0, 1)))
+            random_puzzle.append(new_row)
+
+        for step in range(rand.randint(1, self.nrows * self.ncols)):
+            random_touchpoints.append(
+                (
+                    rand.randint(0, self.nrows * self.ncols - 1),
+                    rand.randint(0, self.nrows * self.ncols - 1),
+                )
+            )
+
+        return State(random_puzzle, random_touchpoints)
+
     def neighbors(self, state):
-        """ return a list of all neighbors of the given state.
-    
-       
-        """
+        """return a list of all neighbors of the given state."""
         # list of states that are neighbors to the given state
-        neighbors = [] 
-        
-        #TODO: Implement this
-  
-        return neighbors        
-        
+        neighbors = []
+
+        # TODO: Implement this
+
+        return neighbors
+
     def random_step(self, state):
-        """ Return a State that is a random neighbour of the given State.
-            :param: state: A State object
+        """Return a State that is a random neighbour of the given State.
+        :param: state: A State object
         """
-        #TODO: Implement this
+        # TODO: Implement this
         return None
-        
-        
+
     def best_step(self, state):
-        """ Return the best neighbouring State for the given State.
-            It doesn't have to be better than the given State!
-            :param: state: A State object
+        """Return the best neighbouring State for the given State.
+        It doesn't have to be better than the given State!
+        :param: state: A State object
         """
-        #TODO: Implement this
+        # TODO: Implement this
         return None
 
     def random_better(self, state):
-        """ Return a State that is a random BETTER neighbour of the given State.
-            Should return None if there isn't one!
-            :param: state: A State object
+        """Return a State that is a random BETTER neighbour of the given State.
+        Should return None if there isn't one!
+        :param: state: A State object
         """
-        #TODO: Implement this
+        # TODO: Implement this
         return None
 
 
-  
-
-            
 # end of file
-
